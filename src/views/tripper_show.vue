476032a5f2ef15dd4c1318e59fdb5e4f007a1a70
robocop019 <chrisbowen@comcast.net>
2019-06-12 14:42:02 -0500

    vue index and show pages with nav bar

--- src/views/tripper_show.vue
+++ src/views/tripper_show.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="home">
-    <h1>All Flights</h1>
-    <div v-for="photo in photos">
-      <h2>{{ photo.name }}</h2>
-      <img v-bind:src="photo.url">
-      <p>Width: {{ photo.width }}</p>
-      <p>Height: {{ photo.height }}</p>
+    <div class="container">
+      <table class="table table-striped table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Flight #</th>
+            <th scope="col">Airline</th>
+            <th scope="col">Boarding Time</th>
+            <th scope="col">Departure Time</th>
+            <th scope="col">Departure</th>
+            <th scope="col">Arrival</th>
+            <th scope="col">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">{{flight['id']}}</th>
+            <td> {{flight['airline']}} </td>
+            <td> {{flight['boarding_time']}} </td>
+            <td> {{flight['departure_time']}} </td>
+            <td> {{flight['departure_airport']}} </td>
+            <td> {{flight['arrival_airport']}} </td>
+            <td> {{flight['status']}} </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -16,12 +35,13 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      photos: []
+      flight: []
     };
   },
   created: function() {
-    axios.get("/api/photos").then(response => {
-      this.photos = response.data;
+    axios.get('http://localhost:3000/api/trips/' + this.$route.params.id).then(response => {
+      this.flight = response.data;
+      console.log(flight);
     });
   },
   methods: {}
